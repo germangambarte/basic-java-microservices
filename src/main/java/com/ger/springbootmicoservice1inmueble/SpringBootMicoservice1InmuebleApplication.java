92c2fbb4d3117fe459f43a596af52f8b3680fa5b
German Gambarte <germangambarte@gmail.com>
2025-02-14 16:48:19 -0300

    initial commit

---
+++ src/main/java/com/ger/springbootmicoservice1inmueble/SpringBootMicoservice1InmuebleApplication.java
@@ -0,0 +1,13 @@
+package com.ger.springbootmicoservice1inmueble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicoservice1InmuebleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMicoservice1InmuebleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ger/springbootmicoservice1inmueble/SpringBootMicoservice1InmuebleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ger.springbootmicoservice1inmueble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicoservice1InmuebleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
